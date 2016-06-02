a931358f685e0448393def6570695c27fe6e3b13
KARAN <kmodi4@gmail.com>
2016-06-02 13:12:35 +0530

    Initial commit

---
+++ app/src/main/java/com/karan/googlelogin/SignOutG.java
@@ -0,0 +1,13 @@
+package com.karan.googlelogin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignOutG extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_out_g);
+    }
+}
